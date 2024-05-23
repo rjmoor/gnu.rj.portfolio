429016826444d90eb425e7d7f111e8da0b0ab3b9
PIAAR <PIAAR@rmasters.group>
2024-05-23 08:28:23 -0400

    Update imgae

--- src/components/pages/welcomeMaster.jsx
+++ src/components/pages/welcomeMaster.jsx
@@ -1,13 +1,15 @@
 import { useState, useEffect } from 'react';
 import { FaGithub, FaLinkedin, FaTiktok } from 'react-icons/fa';
 import '../styles/welcomeMaster.css';
-import ProfileBackground from '../../assets/Ra-effortless.jpg';
+import ProfileBackground from '../../assets/smooth-ai.jpg';
 import { projectInfo } from '../cards/projects/projects'
 import { simpleNavLinks } from '../links/navigationLinks'
 
 function WelcomeMaster() {
     const [contact, setContact] = useState({ name: '', email: '', message: '' });
     const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
+    const [barWidths, setBarWidths] = useState({ engineering: 50, frontend: 50, backend: 50 });
+    const [error, setError] = useState('');
 
     const handleInputChange = (event) => {
         setContact({ ...contact, [event.target.name]: event.target.value });
@@ -15,13 +17,31 @@ function WelcomeMaster() {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert('Form submitted successfully!');
-    // Handle form submission logic here
+        if (contact.name && contact.email && contact.message) {
+            alert('Form submitted successfully!');
+            // Handle form submission logic here
+        } else {
+            setError('All fields are required.');
+        }
     };
 
     useEffect(() => {
         const handleMouseMove = (event) => {
-            setCursorPos({ x: event.clientX, y: event.clientY });
+            const x = event.clientX;
+            const y = event.clientY;
+            setCursorPos({ x, y });
+            // setCursorPos({ x: event.clientX, y: event.clientY });
+            const windowWidth = window.innerWidth;
+            const windowHeight = window.innerHeight;
+            const newWidthEngineering = (x / windowWidth) * 100;
+            const newWidthFrontend = (y / windowHeight) * 100;
+            const newWidthBackend = ((x + y) / (windowWidth + windowHeight)) * 100;
+
+            setBarWidths({
+                engineering: newWidthEngineering,
+                frontend: newWidthFrontend,
+                backend: newWidthBackend,
+            });
         };
 
         window.addEventListener('mousemove', handleMouseMove);
@@ -48,10 +68,10 @@ function WelcomeMaster() {
 
             </section>
             <section className="hero-section">
-            {/* <div className="tooltip-container">
+            <div className="tooltip-container">
                     Hover over me!
                 <span className="tooltip-text">Tooltip text goes here</span>
-            </div> */}
+            </div>
                 
                 <div className="grid-hero-content">
                     <div className='kicker-heading'>
@@ -80,6 +100,7 @@ function WelcomeMaster() {
                                 <label htmlFor="message">Please write a message</label>
                                 <textarea id="message" name="message" value={contact.message} onChange={handleInputChange} placeholder='I would like to know more about you...'></textarea>
                             </div>
+                            {error && <p className="error">{error}</p>}
                             <button type="submit">Send</button>
                         </form>
                     </div>
@@ -105,12 +126,18 @@ function WelcomeMaster() {
                                     <p className="about-text">
                                         Allow me to introduce myself - I am <span className="text-l">R J Moore</span>, a highly versatile professional with a passion for <span className="text-l">customer service</span>, <span className="text-l">engineering</span>, and <span className="text-l">web development</span>. With <span className="text-l">15 years of experience</span> in the field of customer service, I have developed a keen eye for detail, a strong commitment to client satisfaction, and exceptional communication skills. Throughout my career, I have consistently surpassed expectations, ensuring that each customer interaction is a positive and enriching experience. For the past <span className="text-l">decade</span>, I have worked as an <span className="text-l">Engineering Technician</span>, specializing in the fascinating realm of <span className="text-l">automation</span> and <span className="text-l">robotics</span>. This field has allowed me to acquire an extensive skill set, working with state-of-the-art technologies, troubleshooting complex issues, and implementing innovative solutions. My deep understanding of engineering principles and ability to navigate intricate systems propelled me to the forefront of my field.
                                     </p>
+                                <div className="skill-bar-container">
+                                    <div className="skill-bar" style={{ width: `${barWidths.engineering}%` }}></div>
+                                </div>
                                 </article>
                                 <article>
                                     <h3>Front-end</h3>
                                     <p className="about-text">
                                         Driven by a desire to embrace new challenges and expand my horizons, I successfully transitioned into my current role as a <span className="text-l">Front End Web Author</span>. Drawing on my technical acumen and creative flair, I now have the privilege of crafting captivating and user-friendly web experiences. By seamlessly blending aesthetics and functionality, I create web solutions that engage users on multiple platforms. My proficiency in <span className="text-l">HTML</span>, <span className="text-l">CSS</span>, and <span className="text-l">JavaScript</span> enables me to bring ideas to life and deliver exceptional results.
-                                    </p>
+                                </p>
+                                <div className="skill-bar-container">
+                                    <div className="skill-bar" style={{ width: `${barWidths.frontend}%` }}></div>
+                                </div>
                                 </article>
                                 <article>
                                     <h3>Back-end</h3>
@@ -118,13 +145,16 @@ function WelcomeMaster() {
                                         Furthermore, I am formally trained as a <span className="text-l">Python Technical Consultant</span>, augmenting my skill set and providing me with a comprehensive understanding of back-end web development. This expertise allows me to bridge the gap between front-end design and back-end functionality, ensuring cohesive and robust web solutions that meet and exceed expectations.
 
                                         With my diverse background in customer service, engineering, and web development, I bring a unique perspective and a multifaceted skill set to any project. I thrive in collaborative environments and am committed to delivering excellence in every endeavor. My passion for continuous improvement, combined with my adaptability to diverse roles, makes me an invaluable asset to any team.
-                                    </p>    
+                                </p>    
+                                <div className="skill-bar-container">
+                                    <div className="skill-bar" style={{ width: `${barWidths.backend}%` }}></div>
+                                </div>
                                 </article>
                                 <article>
                                     <h3>Summary</h3>
                                     <p className="about-text">
                                         In summary, I am <span className="text-l">R J Moore</span> - a consummate professional with <span className="text-l">15 years of experience</span> in customer service, a <span className="text-l">decade of expertise</span> as an Engineering Technician in automation and robotics, and a current focus on front-end web development as a Web Developer. With a proven track record of success across multiple industries, I am poised to make a significant impact and thrive in any professional endeavor.
-                                    </p>
+                                </p>
                                 </article>    
                             </div>
                 </div>
